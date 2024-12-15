454a8b1c629ee13f9f1bd687c7b4209ad6335ffd
Muhammed Can Çetin <68066318+mcancetin@users.noreply.github.com>
2024-12-16 00:17:43 +0300

    Enhance Tailwind CSS configuration with new color palette, font families, font sizes, and background image

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,32 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "dark-navy": "#070724",
+        "charcoal-gray": "#38384F",
+        "light-gray": "#838391",
+        "sky-blue": "#419EBB",
+        "golden-yellow": "#EDA249",
+        "vibrant-purple": "#6D2ED5",
+        "bright-red": "#D14C32",
+        "deep-red": "#D83A34",
+        "burnt-orange": "#CD5120",
+        "teal-green": "#1EC2A4",
+        "bright-blue": "#2D68F0",
+      },
+      fontFamily: {
+        antonio: ["var(--font-antonio)", "sans"],
+        spartan: ["var(--font-spartan)", "sans"],
+      },
+      fontSize: {
+        h1: ["5rem", "6.75rem"], // 80px -> 5rem, 108px -> 6.75rem
+        h2: ["2.5rem", "3.25rem"], // 40px -> 2.5rem, 52px -> 3.25rem
+        h3: ["0.75rem", "1.5625rem"], // 12px -> 0.75rem, 25px -> 1.5625rem
+        h4: ["0.6875rem", "1.5625rem"], // 11px -> 0.6875rem, 25px -> 1.5625rem
+        body: ["0.875rem", "1.5625rem"], // 14px -> 0.875rem, 25px -> 1.5625rem
+      },
+
+      backgroundImage: {
+        stars: "url('/assets/background-stars.svg')",
       },
     },
   },
