a5857cb778fc12ebc73287a2ee4b0d7367998fac
Muhammed Can Çetin <68066318+mcancetin@users.noreply.github.com>
2024-12-18 10:06:55 +0300

    Refactor button component to use utility function for planet colors

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,15 +1,5 @@
+import { getPlanetColor } from "@/utils/getPlanetColor";
 import { MouseEventHandler } from "react";
-
-const colorVariants: { [key: string]: string } = {
-  mercury: "bg-sky-blue",
-  venus: "bg-golden-yellow",
-  earth: "bg-vibrant-purple",
-  mars: "bg-bright-red",
-  jupiter: "bg-deep-red",
-  saturn: "bg-burnt-orange",
-  uranus: "bg-teal-green",
-  neptune: "bg-bright-blue",
-};
 
 type ButtonProps = {
   number: string;
@@ -26,10 +16,11 @@ export default function Button({
   isActive,
   onClick,
 }: ButtonProps) {
+  const color = getPlanetColor(planet);
   return (
     <button
       onClick={onClick}
-      className={`${isActive ? colorVariants[planet] : ""} heading-3 inline-flex w-full gap-7 border border-white/20 px-7 py-3 hover:bg-[#D8D8D8] hover:bg-opacity-20`}
+      className={`${isActive ? color : ""} heading-3 inline-flex w-full gap-7 border border-white/20 px-7 py-3 hover:bg-[#D8D8D8] hover:bg-opacity-20`}
     >
       <span className="text-white/50">{number}</span>
       <span className="heading-3 uppercase">{label}</span>
